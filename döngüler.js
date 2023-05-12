c7d2a05072b5653b9b8a978bc7f6f3a52f3ab779
kplnhsyn <kplnhsyn.49@gmail.com>
2023-05-12 18:37:43 +0200

    döngüler

---
+++ döngüler.js
@@ -0,0 +1,93 @@
+// soru-1 1000 ile 2000 arasındaki 13 e bölümünden kalanı 3 olan sayıları konsola yazdırınız ?
+
+// let a = [1, 2, 3, 4, 5]
+// let sayac = 0
+
+// for (let i = 1000; i < 2000; i++) {
+//   if (i % 13 == 3) {
+//     console.log(i)
+//     sayac++
+//   }
+// }
+// console.log(sayac)
+
+// soru-2 kullanıcıdan alınan 5 adet sayının ortalamasını konsola yazdırınız ?
+
+// let sayac = 0
+// let toplam = 0
+
+// while (sayac < 5) {
+//   let sayi = +prompt('Lütfen Bir sayı giriniz')
+//   toplam += sayi
+
+//   sayac++
+// }
+
+// console.log(toplam / sayac);
+
+// soru-3 Kullanıcıdan alınan sayıları negatif girilene kadar toplayan ve sonucunu konsola yazdıran kodu yazınız ?(while)
+
+// let durum = true
+// let sum = 0
+
+// while (durum) {
+//   let sayi = +prompt('Sayı giriniz')
+
+//   if (sayi < 0) {
+
+//     durum = false
+//     continue
+//   }
+
+//   sum += sayi
+// }
+
+// console.log(sum)
+
+// let durum1 = true
+// let sum2 = 0
+
+// while (durum1) {
+//   let sayi = +prompt('Sayı giriniz')
+
+//   if (sayi < 0) {
+
+//     break
+//   }
+
+//   sum2 += sayi
+// }
+
+// console.log(sum2)
+
+// soru-4 sizden 1'den 100'e kadar sayıları konsola yazdırmanız istenmektedir ancak 3'ün katlarına gelince 'Fizz', 5'in katlarına glince 'Buzz' yazacak. Hem 'ün hemde 5'in katına geldiğinde ise 'FizzBuzz' yazdırmanız istenmektedir.
+
+// soru-5 Bir sayının(number) basamak sayısını yazdıran kodu yazınız ?(Math.floor)
+
+// soru-6 Kullanıcıdan alınan n adet kadar olan Fibonacci dizisini aralarında boşluk olan string şekilde konsola yazdırınız ?  // 0 1 1 2 3 5 8 13
+
+// soru-7 Aşağıdaki çıktıyı konsola yazdırınız
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+let stars = "";
+for (let i = 0; i < 5; i++) {
+  stars += "\n";
+  for (let j = 0; j < 5; j++) {
+    stars += "* ";
+  }
+}
+console.log(stars);
+
+// soru-8 Aşağıdaki çıktıyı konsola yazdırınız ?
+
+// 0
+// 00
+// 000
+// 0000
+// 00000
+// 000000
--- if_else.js
+++ if_else.js
@@ -1,34 +1,34 @@
 // 1- 18 yaşından büyüklerin oy kullanabileceğini 18 yaşından küçüklerin oy kullanamayacağını ternary yapı ile konsola yazdıran kodu yazınız. (ternary)
-​
-const a = 'asd'
+
+// const a = 'asd'
 // console.log(Boolean(a))
-​
-var status1 = false
-​
+
+// var status1 = false
+
 // if (status1) {
 //   console.log('deneme')
 // }
 // console.log('deneme1')
-​
+
 // if (status1) {
 //   console.log('deneme')
 // } else {
 //   console.log('deneme2')
 // }
-​
+
 // status1 ? console.log('durum1') : console.log('durum2')
-​
+
 // let yas = prompt('Yaşınızı giriniz?')
 // console.log(yas)
-​
+
 // if (Number(yas) < 18) {
 //   console.log(`Yaşınız 18'den küçüktür.Yaşınız ${yas}'tir.`)
 // } else {
 //   console.log(`Yaşınız 18 yada 18'den büyüktür: ${yas}`)
 // }
-​
+
 //  console.log(yas < 18 ? '18 yaşından küçük' : '18 yaşından büyük')
-​
+
 // 2- Bir dersten alınan notun harfini aşağıdaki duruma göre konsola yazdırınız?
 //  Not:
 //  90'dan büyükse AA.
@@ -39,9 +39,9 @@ var status1 = false
 // - 40'den büyük yada 50'a eşitse CD,
 // - 30'den büyük yada 40'a eşitse DD,
 // - 30'dan küçük yada eşitse FF ,
-​
+
 // let dersNotu = +prompt('Ders notunu giriniz:')
-​
+
 // if (dersNotu < 0 || dersNotu > 100) {
 //   console.log("Notunuz 100'den büyük 0'dan küçük olamaz")
 // } else if (dersNotu > 90) {
@@ -61,16 +61,16 @@ var status1 = false
 // } else {
 //   console.log("Harfiniz FF'dır.")
 // }
-​
+
 // 3- Artık yıl sorusu
 // Artık yıl, bir yıldaki günlerin 365 yerine 366 gün olmasına denir. Bu fazladan gün (artık gün), normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.  Bunun hesaplanması için aşağıdaki algoritma kullanılır.
-​
+
 // Bir tarih 4 ile tam bölünüp 100 katları ile tam bölünemiyorsa, veya yıl 400 sayısına tam bölünüyorsa artık yıldır. BU iki şartın dışında yılarda şubat ayı 28 gün, artık yıllarda 29 gün olarak belirlenir.
-​
+
 // Yukarıdaki bilgilere göre bir yılın artık yıl olup olmadığını konsola yazdıran conditional statement'i yazınız.
-​
+
 // let yil = +prompt('yil giriniz')
-​
+
 // if (yil % 4 == 0 && yil % 100 != 0) {
 //   console.log(`${yil} yılı artık yıldır.`)
 // }
@@ -80,13 +80,13 @@ var status1 = false
 // else {
 //   console.log(`${yil} yılı artık yıl değildir.`)
 // }
-​
+
 // yil % 100 != 0
-​
+
 // if ((yil % 4 == 0) && (yil % 100 != 0) || (yil % 400 == 0) ) {
 //   console.log(`${yil} yılı artık yıldır.`)
 // }
-​
+
 // else {
 //   console.log(`${yil} yılı artık yıl değildir.`)
 // }
